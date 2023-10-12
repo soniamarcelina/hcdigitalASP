faa76db5f57769641cf1f42c21b84fd443af3059
soniamarcel <1910631170047@student.unsika.ac.id>
2023-10-12 16:12:50 +0700

    update-12.10

--- Controllers/MPPController.cs
+++ Controllers/MPPController.cs
@@ -21,44 +21,30 @@ public class MPPController : Controller
      [HttpGet("Index")]
     public IActionResult Index()
     {
-    //    string query = "SELECT Division, Department, PosTitle, id_position FROM tadposition"; // Ganti dengan query SQL Anda
-    //     if (_context.tadposition != null)
-    //     {
-    //         List<Position> results = _context.tadposition.FromSqlRaw(query).ToList();
-    //         return View(results);
-    //     }
-    //     else
-    //     {
-    //         // Handle ketika _context.tadposition bernilai null
-    //         // Misalnya, arahkan ke halaman kesalahan atau tindakan lain sesuai kebutuhan.
-    //         return View("Error");
-    //     }
-    var groupedData = _context.tadposition?
-    .GroupBy(item => item.Division)
-    .Select(group => new
-    {
-        Division = group.Key,
-        Items = group.ToList()
-    })
-    .ToList();
-        return View(groupedData);
-    
-    }
-
-    [HttpGet("Existing")]
-    public IActionResult Existing()
-    {
-        var results = _context.tadposition?
-        .GroupBy(m => new { m.Division, m.Department, m.PosTitle })
+        var res = _context.tadposition?
+        .GroupBy(m => new { m.Division, m.Department })
         .Select(group => new
         {
             Division = group.Key.Division,
             Department = group.Key.Department,
-            PosTitle = group.Key.PosTitle,
             Count = group.Count()
         })
         .ToList();
-        return View(results);
+        return View(res);
+    
+    }
+
+     [HttpGet("Existing")]
+    public IActionResult Existing(string Department)
+    {
+        var positions = _context.tadposition?
+            .Where(m => m.Department == Department)
+            .Select(m => m.PosTitle) 
+            .ToList();
+
+            ViewBag.Department = Department; // Kirim nama departemen ke tampilan
+
+        return View(positions);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
